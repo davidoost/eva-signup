2085f000702680549e05708aaf0b7c1d33df16af
David Oost <david@newblack.io>
2024-10-10 14:41:00 +0200

    env var validation
    Enviroment variables will be validated on startup

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
-/** @type {import('next').NextConfig} */
+const validateEnvironmentVariables = (
+  await import("./src/functions/validate-env-vars.mjs")
+).default;
+
+async function runValidations() {
+  try {
+    await validateEnvironmentVariables(); // Validate environment variables
+  } catch (error) {
+    console.error("❌ Environment validation failed:", error.message);
+    process.exit(1); // Stop the process if validation fails
+  }
+}
+
+runValidations();
+
 const nextConfig = {};
 
 export default nextConfig;
