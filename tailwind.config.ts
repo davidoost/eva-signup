527eb64655953f8a3df34c0d30245b3c342adbfc
David Oost <david@newblack.io>
2024-10-31 22:17:44 +0100

    deploy links

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,6 +17,38 @@ const config: Config = {
     },
   },
   darkMode: "class",
-  plugins: [nextui()],
+  plugins: [
+    nextui({
+      themes: {
+        fenix: {
+          extend: "light", // <- inherit default values from dark theme
+          colors: {
+            primary: {
+              50: "#002B11",
+              100: "#00391A",
+              200: "#00522B",
+              300: "#006C3C",
+              400: "#00864E",
+              500: "#00A260",
+              600: "#00BF73",
+              700: "#00D788",
+              800: "#00F19D",
+              900: "#00F9A8",
+              DEFAULT: "#00A260",
+              foreground: "#ffffff",
+            },
+            focus: "#00BF73",
+          },
+          layout: {
+            radius: {
+              small: "0px",
+              medium: "0px",
+              large: "0px",
+            },
+          },
+        },
+      },
+    }),
+  ],
 };
 export default config;
